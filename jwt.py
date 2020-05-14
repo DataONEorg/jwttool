f7365354f73ee721ec99d4fe2bb43ce152d71a34
Matt Jones <gitcode@magisa.org>
2020-05-13 16:28:09 -0800

    refactor into basic functions

--- jwt.py
+++ jwt.py
@@ -4,45 +4,50 @@
 from jwcrypto import jwt, jwk
 from datetime import datetime, timedelta, timezone
 
-algorithm = 'HS256'
-key = jwk.JWK(generate='oct', size=256)
-print("KEY")
-k = key.export()
-print(k)
+def main():
+    key = read_key("privkey.pem")
+    token = create_token(key)
+    validate(token, key)
 
-algorithm = 'RS256'
-with open("privkey.pem", "rb") as pemfile:
-    key = jwk.JWK.from_pem(pemfile.read())
-k = key.export()
-print(k)
+def read_key(keyfile):
+    with open(keyfile, "rb") as pemfile:
+        key = jwk.JWK.from_pem(pemfile.read())
+    #k = key.export()
+    #print(k)
+    return(key)
 
-consumerKey = "theconsumerkey"
-userId = "http://orcid.org/0000-0003-0077-4738"
-fullName = "Matthew B. Jones"
+def create_token(key):
+    algorithm = 'RS256'
+    consumerKey = "theconsumerkey"
+    userId = "http://orcid.org/0000-0003-0077-4738"
+    fullName = "Matthew B. Jones"
 
-ttl = 18*60*60
-now = datetime.now(timezone.utc)
-expires = now + timedelta(seconds=ttl)
+    ttl = 18*60*60
+    now = datetime.now(timezone.utc)
+    expires = now + timedelta(seconds=ttl)
 
-# standard JWT fields: https://tools.ietf.org/html/rfc7519#section-4.1.4
-Token = jwt.JWT(header={"alg": algorithm},
-                claims={
-                    "sub":userId,
-                    "fullName": fullName,
-                    "issuedAt":now.isoformat(),
-                    "consumerKey": consumerKey,
-                    "exp":expires.timestamp(),
-                    "userId": userId,
-                    "ttl":ttl,
-                    "iat":now.timestamp()
-                })
-Token.make_signed_token(key)
-t = Token.serialize()
-print("TOKEN")
-print(t)
+    # standard JWT fields: https://tools.ietf.org/html/rfc7519#section-4.1.4
+    Token = jwt.JWT(header={"alg": algorithm},
+                    claims={
+                        "sub":userId,
+                        "fullName": fullName,
+                        "issuedAt":now.isoformat(),
+                        "consumerKey": consumerKey,
+                        "exp":expires.timestamp(),
+                        "userId": userId,
+                        "ttl":ttl,
+                        "iat":now.timestamp()
+                    })
+    Token.make_signed_token(key)
+    t = Token.serialize()
+    return(t)
 
-# Convert tokenstring to Token and validate
-ST = jwt.JWT(key=key, jwt=t)
-print("CLAIMS")
-print(ST.claims)
+def validate(token, key):
+    # Convert tokenstring to Token and validate
+    ST = jwt.JWT(key=key, jwt=token)
+    print("CLAIMS")
+    print(ST.claims)
+    return(ST.token.is_valid)
 
+if __name__ == '__main__':
+    main()

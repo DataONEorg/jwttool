1660791a4fddfafbb384f9e0645a388ad832bef2
Matt Jones <gitcode@magisa.org>
2020-05-13 16:41:40 -0800

    factor out create_claims() into function

--- jwt.py
+++ jwt.py
@@ -5,39 +5,40 @@ from jwcrypto import jwt, jwk
 from datetime import datetime, timedelta, timezone
 
 def main():
+    subject = "http://orcid.org/0000-0003-0077-4738"
+    name = "Matthew B. Jones"
+    ttl = 18*60*60
+    claims = create_claims(subject, name, ttl)
     key = read_key("privkey.pem")
-    token = create_token(key)
+    algorithm = 'RS256'
+    token = create_token(algorithm, key, claims)
     validate(token, key)
 
 def read_key(keyfile):
     with open(keyfile, "rb") as pemfile:
         key = jwk.JWK.from_pem(pemfile.read())
-    #k = key.export()
-    #print(k)
     return(key)
 
-def create_token(key):
-    algorithm = 'RS256'
-    consumerKey = "theconsumerkey"
-    userId = "http://orcid.org/0000-0003-0077-4738"
-    fullName = "Matthew B. Jones"
-
-    ttl = 18*60*60
+def create_claims(subject, full_name, ttl=60*60):
+    # standard JWT fields: https://tools.ietf.org/html/rfc7519#section-4.1.4
+    consumerKey = "jwttool"
     now = datetime.now(timezone.utc)
     expires = now + timedelta(seconds=ttl)
+    claims={
+        "sub":subject,
+        "fullName": full_name,
+        "issuedAt":now.isoformat(),
+        "consumerKey": consumerKey,
+        "exp":expires.timestamp(),
+        "userId": subject,
+        "ttl":ttl,
+        "iat":now.timestamp()
+    }
+    return(claims)
 
-    # standard JWT fields: https://tools.ietf.org/html/rfc7519#section-4.1.4
+def create_token(algorithm, key, claims):
     Token = jwt.JWT(header={"alg": algorithm},
-                    claims={
-                        "sub":userId,
-                        "fullName": fullName,
-                        "issuedAt":now.isoformat(),
-                        "consumerKey": consumerKey,
-                        "exp":expires.timestamp(),
-                        "userId": userId,
-                        "ttl":ttl,
-                        "iat":now.timestamp()
-                    })
+                    claims=claims)
     Token.make_signed_token(key)
     t = Token.serialize()
     return(t)
@@ -45,7 +46,6 @@ def create_token(key):
 def validate(token, key):
     # Convert tokenstring to Token and validate
     ST = jwt.JWT(key=key, jwt=token)
-    print("CLAIMS")
     print(ST.claims)
     return(ST.token.is_valid)
 
